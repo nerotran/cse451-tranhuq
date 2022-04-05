0ede6e91009e8c3d3488cf765cf47d6aa9592b9e
Nero Tran <tranhuq@miamioh.edu>
2022-04-05 19:25:52 -0400

    continue

--- app1/database/seeders/DatabaseSeeder.php
+++ app1/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,10 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
          DataSeeder::class
+        ]);
+        DB::table('data')->insert([
+            'key'=>"Hello",
+            'value'=>"scott"
         ]);
     }
 }
