22d77134662a49d0f04638d5f856ef1d9e307d46
Nero Tran <tranhuq@miamioh.edu>
2022-04-05 20:32:13 -0400

    continue

---
+++ app1/resources/views/keyClear.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+    my 451 page
+    <br>
+    <a href="{{ url('/') }}">home page</a>
+    <div>
+    <form action="/cse451-tranhuq-web/app1/public/key/update" method="get">
+        <label for="password">password:</label><br>
+        <input type="input" id="password" name="password"><br>
+        <input type="submit" value="Submit">
+    </form>
+    </body>
+</html>
