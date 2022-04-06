6d3544f0b63d6d3d6578ab1e1a13548e71d0b7fa
Nero Tran <tranhuq@miamioh.edu>
2022-04-05 20:00:40 -0400

    continue

---
+++ app1/resources/views/update.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+    my 451 page
+    <br>
+    <a href="{{ url('/') }}">home page</a>
+    <div>
+    	<h2>Server Info from php</h2>
+    <form action="/update.blade.php" method="post">
+        <label for="key">Key:</label><br>
+        <input type="text" id="key" name="key"><br>
+        <label for="value">value:</label><br>
+        <input type="text" id="value" name="value"><br><br>
+        <input type="submit" value="Submit">
+    </form>
+    </body>
+</html>
