e4114644ea903960cbeed788c8d402d2ce87f40a
Nero Tran <tranhuq@miamioh.edu>
2022-04-18 06:17:52 -0400

    continue.

--- Buildings/resources/views/weather.blade.php
+++ Buildings/resources/views/weather.blade.php
@@ -19,24 +19,27 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+
+        <script>
+            $(document).ready(function() {
+                a=$.ajax({
+                url: "https://tranhuq.451.csi.miamioh.edu/cse451-tranhuq-web/Buildings/public/index.php/api/temp",
+                method: "GET"
+                }).done(function(data) {
+                    console.log(data);
+                    $("#temp").append(data["temp"]);
+                }).fail(function(xhr, status, error) {
+                    document.getElementById("zip").value = "";
+                    $("#errorLog").html("Error - " + xhr.status + ": " + xhr.Message);   
+                }); 
+
+            })
+            
+        </script> 
     </head>
     <body class="antialiased">
         <p id="temp"> Temperature: 
         </p>
-
-        <script>
-            a=$.ajax({
-                url: "https://tranhuq.451.csi.miamioh.edu/cse451-tranhuq-web/Buildings/public/index.php/api/temp",
-                method: "GET"
-
-            }).done(function(data) {
-                console.log(data);
-                $("#temp").append(data["temp"]);
-            }).fail(function(xhr, status, error) {
-                document.getElementById("zip").value = "";
-                $("#errorLog").html("Error - " + xhr.status + ": " + xhr.Message);   
-            });
-        </script> 
 
     </body>
 </html>
