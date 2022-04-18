5e8b4f5387c2598f31704592c3ac5e088ab500e5
Nero Tran <tranhuq@miamioh.edu>
2022-04-18 04:28:17 -0400

    continue.

--- Buildings/routes/web.php
+++ Buildings/routes/web.php
@@ -88,4 +88,39 @@ Route::get('/todoist', function (Request $request) {
 
 });
 
+Route::get('/buildings', function () {
+
+    //this calls in all autoload packages installed via composer
+    require '/var/www/html/cse451-tranhuq-web/Buildings/vendor/autoload.php';
+    $uri = "http://ws.miamioh.edu/api/building/v1"; 
+
+    //create a new client
+    $client = new Client([
+        // Base URI is used with relative requests
+        'base_uri' => $uri,
+        // You can set any number of default request options.
+        'timeout'  => 2.0,
+    ]);
+
+    try {
+      $response = $client->request('GET',"");
+    } catch (Exception $e) {
+      print "There was an error getting the token from todoist";
+    //  header("content-type: text/plain",true);
+     // print_r($e);
+      $a=print_r($e,true);
+      error_log($a);
+      exit;
+    }
+    $body = (string) $response->getBody();
+    $jbody = json_decode($body);
+    if (!$jbody) {
+      error_log("no json");
+      exit;
+    }
+
+    return view('buildings', ['buildings'=>$jbody]);
+
+});
+
 Route::get("/task/{id}", [TaskController::class, "show"]);
