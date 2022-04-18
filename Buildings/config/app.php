ddb41c8ac326d42e598465541e4dc6a12d8fd88b
Nero Tran Huu <tranhuq@miamioh.edu>
2022-04-18 06:45:18 -0400

    continue.

--- Buildings/config/app.php
+++ Buildings/config/app.php
@@ -173,7 +173,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+	App\Providers\RouteServiceProvider::class,
+	Subfission\Cas\CasServiceProvider::class,
 
     ],
 
@@ -196,7 +197,8 @@ return [
         'Auth' => Illuminate\Support\Facades\Auth::class,
         'Blade' => Illuminate\Support\Facades\Blade::class,
         'Broadcast' => Illuminate\Support\Facades\Broadcast::class,
-        'Bus' => Illuminate\Support\Facades\Bus::class,
+	'Bus' => Illuminate\Support\Facades\Bus::class,
+	'Cas' => Subfission\Cas\Facades\Cas::class,
         'Cache' => Illuminate\Support\Facades\Cache::class,
         'Config' => Illuminate\Support\Facades\Config::class,
         'Cookie' => Illuminate\Support\Facades\Cookie::class,
