2384778b07cd6ddf3cb1a421a7d58b34bea0c0bf
Nero Tran <tranhuq@miamioh.edu>
2022-04-18 04:18:41 -0400

    continue.

--- Todoist/routes/web.php
+++ Todoist/routes/web.php
@@ -46,7 +46,7 @@ Route::get('/', function () {
 Route::get('/todoist', function (Request $request) {
 
     //this calls in all autoload packages installed via composer
-    require '/var/www/html/cse451-tranhuq-web/Todoist/vendor/autoload.php'; 
+    require '/var/www/html/cse451-tranhuq-web/Buildings/vendor/autoload.php'; 
     require "password.php";
 
     //base uri -> it is important it end in /
@@ -85,6 +85,40 @@ Route::get('/todoist', function (Request $request) {
     error_log("todoist code -> got access");
 
     return redirect("/");
+
+});
+
+Route::get('/buildings', function (Request $request) {
+
+    //this calls in all autoload packages installed via composer
+    require '/var/www/html/cse451-tranhuq-web/Buildings/vendor/autoload.php'; 
+
+    //create a new client
+    $client = new Client([
+        // Base URI is used with relative requests
+        'base_uri' => $uri,
+        // You can set any number of default request options.
+        'timeout'  => 2.0,
+    ]);
+
+    try {
+      $response = $client->request('GET',"");
+    } catch (Exception $e) {
+      print "There was an error getting the token from todoist";
+    //  header("content-type: text/plain",true);
+     // print_r($e);
+      $a=print_r($e,true);
+      error_log($a);
+      exit;
+    }
+    $body = (string) $response->getBody();
+    $jbody = json_decode($body);
+    if (!$jbody) {
+      error_log("no json");
+      exit;
+    }
+
+    return view('buildings', ['buildings'=>$jbody]);
 
 });
 
