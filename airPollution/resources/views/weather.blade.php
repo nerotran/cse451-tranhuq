7f625849459b6bdfbbb6b00033c62f3b3233ec9b
Nero Tran <tranhuq@miamioh.edu>
2022-04-24 23:25:46 -0400

    continue.

--- airPollution/resources/views/weather.blade.php
+++ airPollution/resources/views/weather.blade.php
@@ -20,6 +20,15 @@
             }
         </style>
         <script src="weather.js"></script>
+        <!-- jQuery library -->
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+
+        <!-- Popper JS -->
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script> 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.1/chart.min.js"></script>
 
     </head>
     <body class="antialiased">
