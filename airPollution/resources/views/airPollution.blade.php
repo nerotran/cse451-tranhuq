02021cf88d8857de5781d0325fb8ea1646921849
Nero Tran <tranhuq@miamioh.edu>
2022-04-24 23:31:17 -0400

    continue.

--- airPollution/resources/views/airPollution.blade.php
+++ airPollution/resources/views/airPollution.blade.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
 
 	<!-- jQuery library -->
-	<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
 
 	<!-- Popper JS -->
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
--- airPollution/resources/views/weather.blade.php
+++ airPollution/resources/views/weather.blade.php
@@ -20,7 +20,7 @@
             }
         </style>
         <!-- jQuery library -->
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
 
         <!-- Popper JS -->
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
