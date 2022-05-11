2a411d4b6319b887151440f72a7ebad4794c4a0b
Nero Tran <tranhuq@miamioh.edu>
2022-05-11 02:23:00 -0400

    continue.

--- testings/routes/web.php
+++ testings/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Hello', function () {
+    return view('hello', ['name'=>'Nero']);
+});
