439ebacf72ecf633cbc847b623b70cc55e149809
Nero Tran <tranhuq@miamioh.edu>
2022-05-11 04:23:58 -0400

    continue.

--- airPollution/app/Http/Controllers/WeatherController.php
+++ airPollution/app/Http/Controllers/WeatherController.php
@@ -8,8 +8,14 @@ use Illuminate\Support\Facades\Cache;
 
 class WeatherController extends Controller
 {
-    public function hello()
+    public function getTemp()
     {
+        require '/var/www/html/cse451-tranhuq-web/airPollution/vendor/autoload.php'; 
+        $APIKEY = env('OPENWEATHER_API_KEY','');
+        if ($APIKEY == "") {
+              die ("API KEY NOT DEFINED");
+        }
+
         header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: GET,POST,PUSH,OPTIONS");
         header("content-type: application/json");
@@ -26,10 +32,82 @@ class WeatherController extends Controller
             exit;
         }
 
-        $r['message'] = "Hello World!";
 
-        sendJson("OK",$r);
+        //base uri -> it is important it end in /
+        $uri = "https://api.openweathermap.org/data/2.5/weather?zip=45056,US&units=imperial&appid=";
+        $uri = $uri . $APIKEY;
 
+        //parse parts
+        if (isset($_SERVER['PATH_INFO'])) {
+          $parts = explode("/",$_SERVER['PATH_INFO']);
+          //sanitize
+          for ($i=0;$i<count($parts);$i++) {
+            $parts[$i] = htmlspecialchars($parts[$i]);
+          }
+        } else {
+          $parts = array();
+        }
+
+        array_shift($parts);  //get rid of first part of url which is bogus
+        //get method type
+        //
+
+        //check api/v1 and shift off stack
+        if (sizeof($parts) <2 || $parts[0] != "api") {
+          sendJson("FAIL","Bad Request not /api",[]);
+        }
+        array_shift($parts);
+
+        $method = strtolower($_SERVER['REQUEST_METHOD']);
+
+        if ($method == "options") {
+          sendJson("OK","",[]);
+        }
+
+        if ($method=="get" &&  sizeof($parts) == 1 && $parts[0] == "temp") {
+            
+
+            If (Cache::has("temp")) {
+                $temp = Cache::get("temp");
+                $r['status'] = "CACHE";
+                $r['temp'] = $temp;
+                sendJson("OK",$r);
+            } else {
+
+                //create a new client
+                $client = new Client([
+                    // Base URI is used with relative requests
+                    'base_uri' => $uri,
+                    // You can set any number of default request options.
+                    'timeout'  => 2.0,
+                ]);
+
+                try {
+
+                    $response = $client->request('GET','');
+                } catch (Exception $e) {
+                  $r['status'] = "FAIL";
+                  $r['error'] = $e;
+                  sendJson("FAIL",$r);
+                }
+                $body = (string) $response->getBody();
+                $jbody = json_decode($body);
+                if (!$jbody) {
+                  error_log("no json");
+                }
+
+                $temp = $jbody->main->temp;
+                
+                //store temp in the cache
+                Cache::put("temp",$temp,$seconds=15); //this will tell laravel to cache this object for 15 seconds.
+
+                $r['status'] = "LIVE";
+                $r['temp'] = $temp;
+                sendJson("OK",$r);
+            }
+        }
+        $r['status'] = "FAIL";
+        sendJson("FAIL",$r);
     }
     public function getPollution($city,$state = null,$country = null)
     {
