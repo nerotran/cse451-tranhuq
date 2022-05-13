f10a891f1ff9cfab882adca05f66db9b43a1c34a
tranhuq <tranhuq@miamioh.edu>
2022-05-13 08:58:17 -0400

    Update Final-AJAX/resources/views/final.js

---
+++ Final-AJAX/resources/views/final.js
@@ -0,0 +1,30 @@
+var URL="/api/data";
+
+function get(key, token) {
+	return new Promise((resolv,rej) =>{
+		$.ajax({
+		 url:URL,
+		 method: "GET",
+		 success: function (data) {
+        	resolv(data.numbers)
+      	 },
+      	 error: function (error) {
+        	rej(error)
+      	 },
+		})
+	})
+}
+
+async function program() {
+	var numbers = [];
+	numbers = await get();
+	numbers.foreach((num) => {
+        $("#table").append("<tr><td>" + num + "</td></tr>");
+    });
+
+}
+
+$(document).ready(function() {
+    program();
+});
+
