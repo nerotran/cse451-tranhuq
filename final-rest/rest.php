25eca6cb10b9cc5598172ed4060f5823c0e2f4cd
tranhuq <tranhuq@miamioh.edu>
2022-05-13 09:42:54 -0400

    Update final-rest/rest.php

--- final-rest/rest.php
+++ final-rest/rest.php
@@ -29,7 +29,7 @@ function delete() {
 function update($name) {
   global $conn;
 
-  $sql = "update final set name=$name";
+  $sql = "update final set names=$name";
   if (!$stmt = $conn->prepare($sql)) {
     error_log("Error in sql " . $stmt->error);
     return "Failed, bad sql";
