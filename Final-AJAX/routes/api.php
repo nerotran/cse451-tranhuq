914342a06279b9fe911cd6b0d7e4b2aa36047a44
tranhuq <tranhuq@miamioh.edu>
2022-05-13 09:01:12 -0400

    Update Final-AJAX/routes/api.php

--- Final-AJAX/routes/api.php
+++ Final-AJAX/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers;
+use App\Http\Controllers\RandomController;
 
 /*
 |--------------------------------------------------------------------------
