afc642a1d9e7e08f61f3129e45002e958c329e52
tranhuq <tranhuq@miamioh.edu>
2022-05-13 09:11:02 -0400

    Update Final-AJAX/resources/views/welcome.blade.php

--- Final-AJAX/resources/views/welcome.blade.php
+++ Final-AJAX/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script>
-            var URL="cse451-tranhuq-web/Final-AJAX/public/api/data";
+            var URL="public/api/data";
 
             function get(key, token) {
                 return new Promise((resolv,rej) =>{
