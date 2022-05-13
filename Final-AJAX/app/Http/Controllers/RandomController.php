847406374d7159c35d49e3592771a47ecae055bd
tranhuq <tranhuq@miamioh.edu>
2022-05-13 08:49:17 -0400

    Update Final-AJAX/app/Http/Controllers/RandomController.php

--- Final-AJAX/app/Http/Controllers/RandomController.php
+++ Final-AJAX/app/Http/Controllers/RandomController.php
@@ -6,5 +6,30 @@ use Illuminate\Http\Request;
 
 class RandomController extends Controller
 {
-    //
+    public function getRandom()
+    {
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: GET");
+        header("content-type: application/json");
+        header("Access-Control-Allow-Headers: Content-Type");
+
+        function sendJson($status,$result) {
+              $returnData = array();
+              foreach ($result as $k=>$v) {
+                $returnData[$k] = $v;
+              }
+
+            print json_encode($returnData);
+            exit;
+        }
+
+        $numbers =  array();
+        for ($x = 0; $x <= rand(0,9),$x++) {
+            $numbers[] = rand();
+        }
+
+        r["numbers"] = $numbers;
+        sendJson("OK", $numbers);
+    }
+
 }
